db0d6cdd8c7611fcb653351df5f75fbe939f3200
AidanMonk <AidanMonk@outlook.com>
2025-04-07 14:53:02 -0400

    fixing preset puzzles

--- Backend/KakuroApi/utils/BoardGenerator.py
+++ Backend/KakuroApi/utils/BoardGenerator.py
@@ -8,10 +8,10 @@ class BoardGenerator:
     def generate_easy_board():
         """Generate a simple 4x4 board for beginners"""
         return [
-            [BlockCell(), BlockCell(), SumCell(16, None), SumCell(10, None)],
-            [BlockCell(), SumCell(16, 13), NumberCell(0), NumberCell(0)],
-            [SumCell(None, 12), NumberCell(0), NumberCell(0), NumberCell(0)],
-            [SumCell(None, 8), NumberCell(0), NumberCell(0), BlockCell()]
+            [BlockCell(), SumCell(6,None), SumCell(19, None), BlockCell()],
+            [SumCell(None, 14), NumberCell(0), NumberCell(0), SumCell(5, None)],
+            [SumCell(None, 10), NumberCell(0), NumberCell(0), NumberCell(0)],
+            [BlockCell(), SumCell(None, 6), NumberCell(0), NumberCell(0)]
         ]
 
         # Solution: [[0, 0, 0, 0], [0, 0, 7, 6], [0, 5, 3, 4], [0, 3, 5, 0]]
@@ -20,11 +20,11 @@ class BoardGenerator:
     def generate_medium_board():
         """Generate a 5x5 board with moderate complexity"""
         return [
-            [BlockCell(), BlockCell(), BlockCell(), SumCell(16, None), SumCell(24, None)],
-            [BlockCell(), SumCell(23, None), SumCell(16, 16), NumberCell(0), NumberCell(0)],
-            [SumCell(None, 16), NumberCell(0), NumberCell(0), NumberCell(0), NumberCell(0)],
-            [SumCell(None, 17), NumberCell(0), NumberCell(0), NumberCell(0), NumberCell(0)],
-            [SumCell(None, 14), NumberCell(0), NumberCell(0), BlockCell(), BlockCell()]
+            [BlockCell(), BlockCell(), BlockCell(), SumCell(10, None), SumCell(8, None)],
+            [BlockCell(), BlockCell(), SumCell(8, 6), NumberCell(0), NumberCell(0)],
+            [BlockCell(), SumCell(7, 8), NumberCell(0), NumberCell(0), NumberCell(0)],
+            [SumCell(None, 14), NumberCell(0), NumberCell(0), NumberCell(0), BlockCell()],
+            [SumCell(None, 5), NumberCell(0), NumberCell(0), BlockCell(), BlockCell()]
         ]
 
         # Solution: [7, 9], [3, 5, 2, 6], [9, 4, 1, 3], [5, 9]
